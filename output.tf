3dffc401ebe0f7658830995181ac124a2043df69
Surekha <surekhagidithuri@gmail.com>
2025-06-25 12:11:24 +0530

    export public subnet IDs

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
+#added for testing 
 output "vpc_id" {
   value = aws_vpc.main.id # get the vpc id from vpc declared in vpc.tf
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id #here * is all, we will get a list of subnet ids, here we created 2 public subnets
+}
+
